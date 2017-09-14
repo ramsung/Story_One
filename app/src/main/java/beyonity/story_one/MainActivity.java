bb159baa8f6b47e4f34947e4d8d13ddafd9acd0a
RK <a.r.ramkumar25@gmail.com>
2017-09-14 18:58:06 +0530

    initial

--- app/src/main/java/beyonity/story_one/MainActivity.java
+++ app/src/main/java/beyonity/story_one/MainActivity.java
@@ -52,5 +52,4 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
-
 }
