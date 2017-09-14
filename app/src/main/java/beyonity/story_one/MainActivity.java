cfef3365f5e1aca94e0ad52991a935582a45043d
RK <a.r.ramkumar25@gmail.com>
2017-09-14 18:15:38 +0530

    initial

--- app/src/main/java/beyonity/story_one/MainActivity.java
+++ app/src/main/java/beyonity/story_one/MainActivity.java
@@ -5,9 +5,15 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
