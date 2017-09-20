14c55969388817c42bdc00f8ca9574739fcc6ff2
immohanravi <immohanravi@gmail.com>
2017-09-20 17:59:55 +0530

    creating registration

--- app/src/main/java/beyonity/story_one/SettingsFragment.java
+++ app/src/main/java/beyonity/story_one/SettingsFragment.java
@@ -24,7 +24,7 @@ public class SettingsFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
+    CardView cardView;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -66,7 +66,16 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_settings,container,false);
+        cardView = (CardView) view.findViewById(R.id.tips_setting);
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(),Tips.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 
 
@@ -108,5 +117,6 @@ public class SettingsFragment extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
 
 }
