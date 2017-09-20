6233f575dd090fd54656cb906ea3bf7549a7576d
RK <a.r.ramkumar25@gmail.com>
2017-09-20 23:21:40 +0530

    initial

--- app/src/main/java/beyonity/story_one/Tips.java
+++ app/src/main/java/beyonity/story_one/Tips.java
@@ -10,4 +10,5 @@ public class Tips extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tips);
     }
+
 }
---
+++ app/src/main/java/beyonity/story_one/TipsWebview.java
@@ -0,0 +1,17 @@
+package beyonity.story_one;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class TipsWebview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tips_webview);
+        WebView wv;
+        wv = (WebView) findViewById(R.id.web_crime);
+        wv.loadUrl("file:///android_assets/crime.html");
+    }
+}
