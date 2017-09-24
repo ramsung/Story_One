2062ec02aabb490720bcef4da924662006f03810
RK <a.r.ramkumar25@gmail.com>
2017-09-24 19:05:03 +0530

    initial

---
+++ app/src/main/java/beyonity/story_one/ReadingActivity.java
@@ -0,0 +1,13 @@
+package beyonity.story_one;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reading);
+    }
+}
